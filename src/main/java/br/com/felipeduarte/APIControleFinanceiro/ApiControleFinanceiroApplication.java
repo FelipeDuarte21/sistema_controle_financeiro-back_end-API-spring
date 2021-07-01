82e00b7e99dc96411527e9e043fcb8361a80ac8d
Felipe Duarte <felipe15lfde@gmail.com>
2021-07-01 03:30:55 -0300

    Base do Projeto

---
+++ src/main/java/br/com/felipeduarte/APIControleFinanceiro/ApiControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipeduarte.APIControleFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipeduarte/APIControleFinanceiro/ApiControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipeduarte.APIControleFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
