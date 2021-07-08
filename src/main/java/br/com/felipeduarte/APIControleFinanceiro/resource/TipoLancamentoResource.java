5dce633495c46cf961f644d753b168bbf6a525fa
Felipe Duarte <felipe15lfde@gmail.com>
2021-07-08 00:59:43 -0300

    Permitindo o acesso aos endpoints de tipos de lançamentos

--- src/main/java/br/com/felipeduarte/APIControleFinanceiro/resource/TipoLancamentoResource.java
+++ src/main/java/br/com/felipeduarte/APIControleFinanceiro/resource/TipoLancamentoResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import br.com.felipeduarte.APIControleFinanceiro.model.TipoLancamento;
 import br.com.felipeduarte.APIControleFinanceiro.resource.exception.ObjectNotContentException;
 import br.com.felipeduarte.APIControleFinanceiro.service.TipoLancamentoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/tipoLancamento")
 public class TipoLancamentoResource {
