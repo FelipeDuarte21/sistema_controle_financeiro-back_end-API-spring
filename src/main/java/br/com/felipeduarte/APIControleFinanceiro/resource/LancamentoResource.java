7d55d3e4b251651c5025627aeaa6ecd63214ca8f
Felipe Duarte <felipe15lfde@gmail.com>
2021-07-07 22:35:05 -0300

    Permitindo acesso aos endpoints de lancamento

--- src/main/java/br/com/felipeduarte/APIControleFinanceiro/resource/LancamentoResource.java
+++ src/main/java/br/com/felipeduarte/APIControleFinanceiro/resource/LancamentoResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import br.com.felipeduarte.APIControleFinanceiro.resource.exception.ObjectBadReq
 import br.com.felipeduarte.APIControleFinanceiro.resource.exception.ObjectNotContentException;
 import br.com.felipeduarte.APIControleFinanceiro.service.LancamentoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/lancamento")
 public class LancamentoResource {
