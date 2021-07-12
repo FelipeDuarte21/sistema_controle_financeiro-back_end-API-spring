bec19dc66bc96b00d8558e9111542aa9706a0a1d
Felipe Duarte <felipe15lfde@gmail.com>
2021-07-11 22:53:14 -0300

    Tratando erro de token inválido

--- src/main/java/br/com/felipeduarte/APIControleFinanceiro/security/JWTUtil.java
+++ src/main/java/br/com/felipeduarte/APIControleFinanceiro/security/JWTUtil.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 
 @Component
 public class JWTUtil {
@@ -30,10 +31,15 @@ public class JWTUtil {
 	
 	public String validarToken(String token) {
 		
-		String usuario = JWT.require(Algorithm.HMAC512(this.assinatura))
-				.build().verify(token).getSubject();
-		
-		return usuario;
+		try {
+			String usuario = JWT.require(Algorithm.HMAC512(this.assinatura))
+					.build().verify(token).getSubject();
+			
+			return usuario;
+			
+		}catch(TokenExpiredException e) {
+			return null;
+		}
 		
 	}
 	
