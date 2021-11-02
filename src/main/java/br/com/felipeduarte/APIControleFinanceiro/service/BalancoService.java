aa2baea40c238f2f301400a8d4c8465ceb18aeda
Felipe Duarte <felipe15lfde@gmail.com>
2021-11-02 20:05:47 -0300

    Modificando o código para ajustar o balanco automaticamente

--- src/main/java/br/com/felipeduarte/APIControleFinanceiro/service/BalancoService.java
+++ src/main/java/br/com/felipeduarte/APIControleFinanceiro/service/BalancoService.java
@@ -2,7 +2,6 @@ package br.com.felipeduarte.APIControleFinanceiro.service;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +40,49 @@ public class BalancoService {
 		
 		Balanco balanco = this.repository.findByCategoriaAndFechado(categoria, false);
 		
+		LocalDate agora = LocalDate.now(ZoneId.of(TIME_ZONE));
+		
+		//Verifica se mudou de mês - se sim solicita o cadastro do novo mês e fecha o anterior
+		if(agora.getMonthValue() != balanco.getMes()) {
+			
+			balanco.setFechado(true);
+			this.repository.save(balanco);
+			
+			Balanco novo = this.cadastrar(categoria);
+			return novo;
+		}
+		
 		return balanco;
+	}
+	
+	public Balanco cadastrar(Categoria categoria) {
+		
+		LocalDate agora = LocalDate.now(ZoneId.of(TIME_ZONE));
+		
+		LocalDate dataAnterior = agora.minusMonths(1);
+		
+		Balanco balancoAnterior = this.repository.findByCategoriaAndMesAndAno(categoria, 
+				dataAnterior.getMonthValue(), dataAnterior.getYear());
+		
+		double saldoAnterior = 0.0;
+		
+		if(balancoAnterior != null) {
+			saldoAnterior = balancoAnterior.getSaldoAtual();
+		}
+		
+		Balanco balanco = new Balanco();
+		balanco.setId(null);
+		balanco.setMes(agora.getMonthValue());
+		balanco.setAno(agora.getYear());
+		balanco.setSaldoAnterior(saldoAnterior);
+		balanco.setSaldoAtual(saldoAnterior);
+		balanco.setFechado(false);
+		balanco.setCategoria(categoria);
+		
+		balanco = this.repository.save(balanco);
+		
+		return balanco;
+		
 	}
 	
 	public Balanco recuperarPorData(Long idCategoria, Integer mes, Integer ano) {
@@ -71,31 +112,6 @@ public class BalancoService {
 		return balanco.get();
 	}
 	
-	public void cadastrar(Categoria categoria) {
-		
-		LocalDate agora = LocalDate.now(ZoneId.of(TIME_ZONE));
-		
-		LocalDate dataAnterior = agora.minusMonths(1);
-		Balanco balancoAnterior = this.repository.findByCategoriaAndMesAndAno(categoria, 
-				dataAnterior.getMonthValue(), dataAnterior.getYear());
-		double saldoAnterior = 0.0;
-		if(balancoAnterior != null) {
-			saldoAnterior = balancoAnterior.getSaldoAtual();
-		}
-		
-		Balanco balanco = new Balanco();
-		balanco.setId(null);
-		balanco.setMes(agora.getMonthValue());
-		balanco.setAno(agora.getYear());
-		balanco.setSaldoAnterior(saldoAnterior);
-		balanco.setSaldoAtual(saldoAnterior);
-		balanco.setFechado(false);
-		balanco.setCategoria(categoria);
-		
-		balanco = this.repository.save(balanco);
-		
-	}
-	
 	public void atualizarSaldo(Balanco balanco) {
 		
 		Balanco b = this.buscarPorId(balanco.getId());
@@ -121,25 +137,6 @@ public class BalancoService {
 			b.setSaldoAtual(saldoAnterior + proventos - despesas); 
 			
 			this.repository.save(b);
-			
-		}
-		
-	}
-	
-	public void fecharBalancos() {
-		
-		LocalDate hoje = LocalDate.now(ZoneId.of(TIME_ZONE));
-		LocalDate mesPassado = hoje.minusMonths(1);
-		
-		List<Balanco> balancos = this.repository.findByFechadoAndMesAndAno(false,
-				mesPassado.getMonthValue(),mesPassado.getYear());;
-		
-		if(balancos != null) {
-			
-			balancos.forEach(balanco -> {
-				balanco.setFechado(true);
-				this.repository.save(balanco);
-			});
 			
 		}
 		
