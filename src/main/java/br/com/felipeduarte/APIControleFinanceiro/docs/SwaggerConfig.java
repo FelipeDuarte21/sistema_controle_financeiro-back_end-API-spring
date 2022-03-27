7b69565b825bf7cd24223d672094a87f676a733f
Felipe Duarte <felipe15lfde@gmail.com>
2022-03-27 18:35:17 -0300

    Ajustes na configuração da documentação da API

--- src/main/java/br/com/felipeduarte/APIControleFinanceiro/docs/SwaggerConfig.java
+++ src/main/java/br/com/felipeduarte/APIControleFinanceiro/docs/SwaggerConfig.java
@@ -3,7 +3,6 @@ package br.com.felipeduarte.APIControleFinanceiro.docs;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
@@ -11,38 +10,37 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import com.google.common.net.HttpHeaders;
-
-import io.swagger.models.auth.In;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport{
-	
-	@Value("${swagger.base.path}")
-	private String basePath;
 
 	@Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).host(basePath)
+        return new Docket(DocumentationType.SWAGGER_2)
           .select()
           .apis(RequestHandlerSelectors.basePackage("br.com.felipeduarte.APIControleFinanceiro.resource"))
-          .paths(PathSelectors.any())
-          .build().useDefaultResponseMessages(false)
-          .securitySchemes(Arrays.asList(new ApiKey("Token Acess", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
-          .securityContexts(Arrays.asList(securityContext()))
+          .paths(PathSelectors.ant("/**"))
+          .build()
+          .globalOperationParameters(Arrays.asList(
+        		  new ParameterBuilder()
+        		  .name("Authorization")
+        		  .description("Token de acesso")
+        		  .modelRef(new ModelRef("string"))
+        		  .parameterType("header")
+        		  .required(false)
+        		  .build()
+           ))
           .apiInfo(metaData());
     }
 	
@@ -62,25 +60,6 @@ public class SwaggerConfig extends WebMvcConfigurationSupport{
 		.addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**")
 		.addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
-	
-	private SecurityContext securityContext() {
-	    return SecurityContext.builder()
-	        .securityReferences(defaultAuth())
-	        .forPaths(PathSelectors.ant("/api/**"))
-	        .build();
-	}
-
-	List<SecurityReference> defaultAuth() {
-	    AuthorizationScope authorizationScopeAdmin
-	        = new AuthorizationScope("ADMIN", "accessEverything");
-	    AuthorizationScope authorizationScopeUser
-        	= new AuthorizationScope("USER", "accessEverything");
-	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[2];
-	    authorizationScopes[0] = authorizationScopeAdmin;
-	    authorizationScopes[1] = authorizationScopeUser;
-	    return Arrays.asList(
-	        new SecurityReference("Token Access", authorizationScopes));
 	}
 	
 	@Override
